a4ac08293fba3aab8f02f7af87c28f132028a360
Shama Hoque <shama.hoque@gmail.com>
2015-12-01 03:40:06 +0600

    little detail to empty pages

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,11 +1,18 @@
 
 import React from 'react';
+const Paper = require('material-ui/lib/paper');
 
 class About extends React.Component {
 
 	render(){
 		return (
-			 <div className="container marginTop">About</div>
+			 <div className="container marginTop">
+            <Paper zDepth={1} className="mediaPiece">
+            <div className="about"><h4>About</h4>
+            <p>An isomorphic ReactJS application with a Node-Express-MongoDB backend.</p>
+            </div>
+            </Paper>
+            <br/></div>
 		)
 	}
 }
